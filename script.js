b40d4a3ecf286ebbfa2379eb2c335135bff3a858
imonraj27072002 <84785226+imonraj27072002@users.noreply.github.com>
2022-06-13 20:56:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function draggable(id) {
+    let box = document.getElementById(id);
+    let clicked = false;
+
+    box.onmousedown = () => {
+        console.log("ok");
+        clicked = true;
+    };
+
+    document.onmouseup = () => {
+        console.log("ooop");
+        clicked = false;
+    };
+    document.onmousemove = (e) => {
+        if (!clicked) return;
+        if (e.clientX < 50 || e.clientX > screen.width - 50) return;
+        // console.log("helo");
+        // let e = window.event;
+        box.style.left = e.clientX - 50 + "px";
+        box.style.top = e.clientY - 50 + "px";
+    };
+}
