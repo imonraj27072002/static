0e380cc9db8bbefbc423347fe426ed25821819bc
imonraj27072002 <84785226+imonraj27072002@users.noreply.github.com>
2022-06-16 22:46:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,22 +4,22 @@ function draggable(id) {
     let clicked = false;
    
 
-    box.onmousedown = () => {
+    box.onmousedown = () =&gt; {
         console.log("ok");
         clicked = true;
     };
 
-    document.onmouseup = () => {
+    document.onmouseup = () =&gt; {
         console.log("ooop");
         clicked = false;
     };
-    document.onmousemove = (e) => {
+    document.onmousemove = (e) =&gt; {
         if (!clicked) return;
-        if (e.clientX < 50 || e.clientX > screen.width - 50) return;
-        // console.log("helo");
-        // let e = window.event;
+        if (e.clientX &lt; box.clientWidth/2 || e.clientX &gt; screen.width - box.clientWidth/2) return;
+        if (e.clientY &lt; box.clientHeight/2 || e.clientY &gt; screen.width - box.clientHeight/2) return;
+     
         
-        box.style.left = e.clientX - 50 + "px";
-        box.style.top = e.clientY - 50 + "px";
+        box.style.left = e.clientX - box.clientWidth/2 + "px";
+        box.style.top = e.clientY - box.clientHeight/2 + "px";
     };
 }
